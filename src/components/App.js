0b358454269133ba777b1ad2065c9f8242d08a03
NiketSah <117289451+NiketSah@users.noreply.github.com>
2023-10-15 09:45:17 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,29 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import '../styles/App.css';
-import star from '../star.png'
+import star from '../star.png';
+
 const App = () => {
+  const [dimensions, setDimensions] = useState({ width: 300, height: 300 });
+
+  const handleClick = () => {
+    const newDimensions = {
+      width: dimensions.width + 2,
+      height: dimensions.height + 2,
+    };
+    setDimensions(newDimensions);
+  };
+
   return (
     <div id="main">
-      <img src={star}  height="300px" width="300px" />
+      <img
+        src={star}
+        height={`${dimensions.height}px`}
+        width={`${dimensions.width}px`}
+        onClick={handleClick}
+      />
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
+
